bfe08d776ef096c79d256d46d8c622866bbbbf28
Alexander Bazhenov <iam@bash.expert>
2019-06-05 12:05:07 +0600

    temporal charts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,31 +1,8 @@
 class HomeController < UiController
   def index
-    @charts = [
-      {
-        type: :line,
-        title: 'Temporal Teams Statistics',
-        data: Team.all.map do |team|
-          {
-            name: team.title || "Unknown",
-            data: team.flags.group_by_minute(:created_at).count
-          }
-        end
-      },
-      {
-        type: :pie,
-        title: 'Flags By Status',
-        data: Flag.group(:status).count
-      },
-      {
-        type: :pie,
-        title: 'Flags By Exploit',
-        data: Flag.joins(:exploit).group(:title).count
-      },
-      {
-        type: :pie,
-        title: 'Flags By Teams',
-        data: Flag.joins(:team).group(:title).count
-      }
-    ]
+    @temporal_teams_flags_data = Team.temporal_statistics_flags
+    @temporal_exploits_flags_data = Exploit.temporal_statistics_flags
+    @temporal_teams_points_data = Team.temporal_statistics_points
+    @temporal_exploits_points_data = Exploit.temporal_statistics_points
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -5,4 +5,26 @@ class Team < ApplicationRecord
   validates :title, :host, presence: true
 
   enum status: %i[active inactive]
+
+  scope :temporal_statistics_flags, ->() do
+    all.sort_by { |team| -team.flags.count }
+      .first(10)
+      .map do |team|
+        {
+          name: team.title || "Unknown",
+          data: Flag.group_by_minutes(10, aggregation: { type: :count, field: '1' }, where_clause: { team_id: team.id })
+        }
+      end
+  end
+
+  scope :temporal_statistics_points, ->() do
+    all.sort_by { |team| -team.flags.map(&:pts).inject(:+) }
+      .first(10)
+      .map do |team|
+        {
+          name: team.title || "Unknown",
+          data: Flag.group_by_minutes(10, aggregation: { type: :sum, field: :pts }, where_clause: { team_id: team.id })
+        }
+      end
+  end
 end
