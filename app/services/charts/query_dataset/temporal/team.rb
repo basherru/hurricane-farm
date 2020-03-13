b1616792880946c3db4f88ef1a3d1d27f355418a
basherru <iam@bash.expert>
2020-03-13 11:24:43 +0300

    fix temporal charts & :cop:

--- app/services/charts/query_dataset/temporal/team.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Charts::QueryDataset::Temporal::Team < Charts::QueryDataset::Temporal
-  private
-
-  def model_dataset
-    model.all.sort_by { |record| -record.flags.count }.first(rating_size)
-  end
-end
